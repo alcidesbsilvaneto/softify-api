7705e6cf5f6f8972ee8e74415c698f91d93dcc5a
Alcides Bezerra <alcides.bsilvaneto@gmail.com>
2023-05-21 18:02:37 -0300

    Bcrypt password hash

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,18 @@
 import { AppDataSource } from '@/common/database/data-source';
 import { Request, Response } from 'express';
 import { User } from '../user/user.model';
+import bcrypt from "bcrypt";
 
 class AuthController {
   async handleAuthenticateRequest(req: Request, res: Response) {
     const { email, password } = req.body;
     try {
       const user = await AppDataSource.getRepository(User).findOne({
-        where: { email, password_hash: password },
+        where: { email },
       });
       if (!user) throw new Error('User not found');
+      const passwordMatch = await bcrypt.compare(password, user.password_hash); // Compare the received password with the hashed password
+      if(!passwordMatch) return res.status(401).json({ message: 'invalid-password' })
       return res.json({ message: 'logged-in' });
     } catch (e) {
       return res.status(400).json({ message: 'User not found' });
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,17 @@
 import { AppDataSource } from '@/common/database/data-source';
 import { Request, Response } from 'express';
 import { User } from './user.model';
+import bcrypt from "bcrypt";
 
 class UserController {
   async handleCreateRequest(req: Request, res: Response) {
     const { name, email, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10); // Hash the password with bcrypt
     try {
       const user = await AppDataSource.getRepository(User).save({
         name,
         email,
-        password_hash: password,
+        password_hash: passwordHash,
       });
       return res.send({ ok: true, user: { id: user.id, name: user.name } });
     } catch (error) {
